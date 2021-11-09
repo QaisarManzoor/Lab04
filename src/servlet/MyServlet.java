16cd851879cb01ffcb6833c5694260351a87c08a
qaisar.manzoor <qaisar736@gmail.com>
2021-11-09 16:05:05 +0500

    My First Commit

---
+++ src/servlet/MyServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Servlet called");
+        out.println("Data Inserted Successfully");
+        String username = request.getParameter("username");
+        // This is use to redirect to other page
+        out.println(username);
+
+        //response.sendRedirect("servletdisplay");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
